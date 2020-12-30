ba1491db72fac1800e017cb1a4c15dc8f65009e0
Brandon Weiss <brandonweiss2008@gmail.com>
2020-12-30 12:45:13 -0500

    updates

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-  migrationDirectory: "migrations",
-  driver: "pg",
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  database:
-    process.env.NODE_ENV === "test"
-      ? process.env.TEST_DATABASE_NAME
-      : process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASS,
-};
+  "migrationDirectory": "migrations",
+  "driver": "pg",
+  "host": process.env.MIGRATION_DB_HOST,
+  "port": process.env.MIGRATION_DB_PORT,
+  "database": process.env.MIGRATION_DB_NAME,
+  "username": process.env.MIGRATION_DB_USER,
+  "password": process.env.MIGRATION_DB_PASS,
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+}
--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8001,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/TaxHub-2.0',
-  DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/TaxHub-test-2.0',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/TaxHub-test-2.0',
   JWT_SECRET: process.env.JWT_SECRET || 'mysecret',
 }
