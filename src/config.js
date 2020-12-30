99ee0c79e37cae2bdb076d133f429385c24a1718
Brandon Weiss <brandonweiss2008@gmail.com>
2020-12-30 10:44:42 -0500

    getting setup for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: 8001,
+  PORT: process.env.PORT || 8001,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/TaxHub-2.0',
   TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/TaxHub-test-2.0',
